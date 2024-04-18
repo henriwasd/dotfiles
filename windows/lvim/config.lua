797c669400f80b37d9e1bed49a64a2ca870375bd
henri-mabiti <henrique.silva@3ton.com.br>
2024-04-18 14:36:05 -0300

    feat: add config lvim windows

---
+++ windows/lvim/config.lua
@@ -0,0 +1,122 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+vim.cmd('set number relativenumber')
+lvim.format_on_save = true
+lvim.reload_config_on_save = true
+lvim.transparent_window = true
+lvim.builtin.nvimtree.setup.view.width = 50
+lvim.plugins = {
+  {
+    'akinsho/flutter-tools.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim',
+    },
+    config = function()
+      require("flutter-tools").setup({
+        widget_guides = {
+          enabled = true,
+        },
+        lsp = {
+          settings = {
+            lineLength = 160,
+            renameFilesWithClasses = "always",
+            documentation = "full",
+          }
+        },
+        debugger = {
+          enabled = true,
+          run_via_dap = true,
+          register_configurations = function(_)
+            local dap = require("dap")
+            dap.set_log_level("TRACE")
+            dap.configurations.dart = {
+              {
+                type = "dart",
+                request = "launch",
+                name = "Launch flutter",
+                dartSdkPath = "/usr/local/bin/dart",
+                flutterSdkPath = "/usr/local/bin/flutter",
+                program = "lib/main.dart",
+                cwd = "${workspaceFolder}",
+                args = {},
+              },
+            }
+            require("dap.ext.vscode").load_launchjs()
+          end,
+        },
+        dev_log = {
+          enabled = false,
+        },
+
+      })
+    end,
+  },
+  {
+    'editorconfig/editorconfig-vim'
+  },
+  {
+    "ThePrimeagen/vim-be-good"
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    config = function()
+      require("copilot_cmp").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false }
+      })
+    end
+  },
+  { 'mg979/vim-visual-multi' },
+}
+
+local has_words_before = function()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+end
+local on_tab = vim.schedule_wrap(function(fallback)
+  local cmp = require("cmp")
+  if cmp.visible() and has_words_before() then
+    cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+  else
+    fallback()
+  end
+end)
+lvim.builtin.cmp.mapping["<Tab>"] = on_tab
+
+-- Set keymaps to go emplements and references
+vim.api.nvim_set_keymap("n", "gr", "<cmd>lua require'telescope.builtin'.lsp_references()<CR>",
+  { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", { noremap = true, silent = true })
+
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+
+lvim.keys.normal_mode["<S-x>"] = ":BufferKill<CR>"
+lvim.keys.normal_mode["<C-d>"] = "<C-d>zz"
+lvim.keys.normal_mode["<C-u>"] = "<C-u>zz"
+lvim.keys.normal_mode["<C-a>"] = "ggVG"
+
+vim.keymap.set('n', 'gn', ":tabe %<CR>")
+lvim.lsp.buffer_mappings.normal_mode["gr"] = {
+  ":lua require'telescope.builtin'.lsp_references()<cr>",
+}
+
+lvim.lsp.buffer_mappings.normal_mode["gd"] = {
+  ":lua vim.lsp.buf.definition()<cr>",
+  -- ":lua require'telescope.builtin'.lsp_definitions()<cr>",
+}
+lvim.keys.normal_mode["<C-b>"] = ":TmuxNavigateLeft<CR>"
+lvim.lsp.buffer_mappings.normal_mode["gD"] = {
+  ":lua vim.lsp.buf.type_definition()<cr>",
+}
+
+lvim.lsp.buffer_mappings.normal_mode["gf"] = {
+  ":Telescope frecency<cr>",
+}
