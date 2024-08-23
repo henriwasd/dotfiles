c34dfff97a39bb382f2fbda2b5f862e2553af41b
henriwasd <henriwasd@gmail.com>
2024-08-23 15:57:46 -0300

    flutter

--- .config/nvim/lua/plugins/flutter.lua
+++ .config/nvim/lua/plugins/flutter.lua
@@ -1,14 +1,17 @@
 return {
   {
     "akinsho/flutter-tools.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = false,
+    dependencies = { "nvim-lua/plenary.nvim", "stevearc/dressing.nvim" },
     config = function()
       require("flutter-tools").setup({
+        fvm = false,
         lsp = {
           settings = {
             lineLength = vim.o.textwidth,
             renameFilesWithClasses = "always",
             documentation = "full",
+            widget_guides = true,
           },
         },
       })
